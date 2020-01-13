e7b9c97e17b60e35b731b64aae8e7ada0d594632
Rim4i4ok <rim4i4ok@gmail.com>
2020-01-13 23:56:33 +0300

    hello message

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const TelegramBot = require("node-telegram-bot-api");
 
 const { botToken } = require("./settings");
+const { hello } = require("./constants/stickers");
 
 const bot = new TelegramBot(botToken, { polling: true });
 
@@ -9,6 +10,7 @@ const bot = new TelegramBot(botToken, { polling: true });
 bot.on("message", msg => {
 	const chatId = msg.chat.id;
 
+	console.log(msg);
 	// send a message to the chat acknowledging receipt of their message
-	bot.sendMessage(chatId, "Received your message. Thanks!");
+	bot.sendSticker(chatId, hello);
 });
---
+++ constants/stickers.js
@@ -0,0 +1,6 @@
+const smile = "BQADAgAD6wQAAkdEugP-KW20PJY4bAI";
+const hello = "BQADAgAD8gUAAkdEugMKI0RuWYSaAAEC";
+const no = "BQADAgADkQYAAuCjggfFP05VDnLMiAI";
+const unknownCommand = "BQADAgADYwYAAuCjggdbj6ny8mg9DAI";
+
+module.exports = { smile, hello, no, unknownCommand };
